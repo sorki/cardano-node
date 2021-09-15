7d42ffe714900139dd19cbff72602efa5bb5b454
MarcFontaine <MarcFontaine@users.noreply.github.com>
2021-09-15 15:33:55 +0200

    add test-address-gen

---
+++ bench/tx-generator/test/test-address-gen.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main
+where
+
+import Prelude
+import Control.Concurrent.MVar
+import Control.Concurrent
+import Control.Monad
+
+import Cardano.Api
+
+keyAddress :: NetworkId -> SigningKey PaymentKey -> AddressInEra MaryEra
+keyAddress networkId k
+  = makeShelleyAddressInEra
+      networkId
+      (PaymentCredentialByKey $ verificationKeyHash $ getVerificationKey k)
+      NoStakeAddress
+
+-- readTextEnvelopeFromFile "/tmp/Downloads/tf"
+keyEnvelope :: TextEnvelope
+keyEnvelope = TextEnvelope
+  "GenesisUTxOSigningKey_ed25519"
+  "Genesis Initial UTxO Signing Key"
+  "X \ETX|\153m\216\t(\228\250\n6M`X\213\f\190\223\225MX\192\v0\170\FS\213\174:w\150g"
+
+testKey :: SigningKey PaymentKey
+testKey = case deserialiseFromTextEnvelope (AsSigningKey AsGenesisUTxOKey) keyEnvelope of
+  Right k -> castSigningKey k
+  Left err -> error $ show err
+
+main :: IO ()
+main = do
+  let expectedAddress = keyAddress (Testnet $ NetworkMagic 42) testKey
+  putStrLn $ show testKey
+  putStrLn $ show expectedAddress
+  good <- newMVar 0
+  bad <- newMVar 0
+  _ <- forkIO $ runner good bad testKey expectedAddress
+  _ <- forkIO $ runner good bad testKey expectedAddress
+  forever $ do
+    g <- readMVar good
+    b <- readMVar bad
+    putStrLn $ show (g,b)
+    threadDelay 1000000
+
+runner :: MVar Integer -> MVar Integer -> SigningKey PaymentKey -> AddressInEra MaryEra -> IO ()
+runner good bad key expectedAddress = forever $ do
+  addr <- do
+    return $ keyAddress (Testnet $ NetworkMagic 42) key
+  if addr == expectedAddress
+    then modifyMVar_ good (return . succ)
+    else modifyMVar_ bad (return . succ)
