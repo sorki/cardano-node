49fa8136b77c455b807373366fc0999264045a51
Yuriy Syrovetskiy <yuriy.syrovetskiy@iohk.io>
2021-09-16 18:37:36 +0300

    cardano-cli: Add update proposal friendly rendering

--- cardano-cli/test/Test/Golden/TxView.hs
+++ cardano-cli/test/Test/Golden/TxView.hs
@@ -16,11 +16,11 @@ txViewTests :: IO Bool
 txViewTests =
   checkSequential $
     Group "`transaction view` Goldens"
-      [ ("golden_view_byron",   golden_view_byron)
+      [ ("golden_view_byron", golden_view_byron)
       , ("golden_view_shelley", golden_view_shelley)
       , ("golden_view_allegra", golden_view_allegra)
-      , ("golden_view_mary",    golden_view_mary)
-      -- , ("golden_view_alonzo",  golden_view_alonzo)
+      , ("golden_view_mary", golden_view_mary)
+      , ("golden_view_alonzo", golden_view_alonzo)
       ]
 
 golden_view_byron :: Property
@@ -53,7 +53,39 @@ golden_view_shelley :: Property
 golden_view_shelley =
   propertyOnce $
   moduleWorkspace "tmp" $ \tempDir -> do
-    transactionBodyFile <- noteTempFile tempDir "transaction-body-file"
+    updateProposalFile <- noteTempFile tempDir "update-proposal"
+    transactionBodyFile <- noteTempFile tempDir "transaction-body"
+
+    -- Create update proposal
+    void $
+      execCardanoCLI
+        [ "governance", "create-update-proposal"
+        , "--decentralization-parameter", "63/64"
+        , "--epoch", "64"
+        , "--extra-entropy", "c0ffee"
+          --  hash(c0ffee) =
+          --    88f04f011dcded879039ae4b9b20219d9448e5c7b42c2d1f638fb8740e0ab8be
+        , "--genesis-verification-key-file"
+        ,   "test/data/golden/shelley/genesis/verification.key"
+            -- hash = 81cb0bc5b6fbba391e6f7ec3d9271cbea25bcbf907181b7c4d5f8c2f
+        , "--key-reg-deposit-amt", "71"
+        , "--max-block-body-size", "72"
+        , "--max-block-header-size", "73"
+        , "--max-tx-size", "74"
+        , "--min-fee-constant", "75"
+        , "--min-fee-linear", "76"
+        , "--min-pool-cost", "77"
+        , "--min-utxo-value", "78"
+        , "--monetary-expansion", "79/80"
+        , "--number-of-pools", "80"
+        , "--out-file", updateProposalFile
+        , "--pool-influence", "82/83"
+        , "--pool-reg-deposit", "83"
+        , "--pool-retirement-epoch-boundary", "84"
+        , "--protocol-major-version", "85"
+        , "--protocol-minor-version", "86"
+        , "--treasury-expansion", "87/88"
+        ]
 
     -- Create transaction body
     void $
@@ -70,6 +102,7 @@ golden_view_shelley =
         , "--withdrawal"
         ,   "stake_test1up00fz9lyqs5sjks82k22eqz7a9srym9vysjgp3h2ua2v2cm522kg\
             \+42"
+        , "--update-proposal-file", updateProposalFile
         , "--out-file", transactionBodyFile
         ]
 
@@ -142,3 +175,48 @@ golden_view_mary =
       execCardanoCLI
         ["transaction", "view", "--tx-body-file", transactionBodyFile]
     diffVsGoldenFile result "test/data/golden/mary/transaction-view.out"
+
+golden_view_alonzo :: Property
+golden_view_alonzo =
+  propertyOnce $
+  moduleWorkspace "tmp" $ \tempDir -> do
+    updateProposalFile <- noteTempFile tempDir "update-proposal"
+    transactionBodyFile <- noteTempFile tempDir "transaction-body"
+
+    -- Create update proposal
+    void $
+      execCardanoCLI
+        [ "governance", "create-update-proposal"
+        , "--epoch", "190"
+        , "--genesis-verification-key-file"
+        ,   "test/data/golden/alonzo/genesis/verification.key"
+            -- hash = acf9e6314616491ec9e9b9d8c4d4818508f81490affdb99ad1643df2
+        , "--utxo-cost-per-word", "194"
+        , "--price-execution-steps", "195/196"
+        , "--price-execution-memory", "196/197"
+        , "--max-tx-execution-units", "(197, 198)"
+        , "--max-block-execution-units", "(198, 199)"
+        , "--max-value-size", "199"
+        , "--collateral-percent", "200"
+        , "--max-collateral-inputs", "201"
+        , "--out-file", updateProposalFile
+        ]
+
+    -- Create transaction body
+    void $
+      execCardanoCLI
+        [ "transaction", "build-raw"
+        , "--alonzo-era"
+        , "--tx-in"
+        ,   "ed7c8f68c194cc763ee65ad22ef0973e26481be058c65005fd39fb93f9c43a20\
+            \#212"
+        , "--fee", "213"
+        , "--update-proposal-file", updateProposalFile
+        , "--out-file", transactionBodyFile
+        ]
+
+    -- View transaction body
+    result <-
+      execCardanoCLI
+        ["transaction", "view", "--tx-body-file", transactionBodyFile]
+    diffVsGoldenFile result "test/data/golden/alonzo/transaction-view.out"
